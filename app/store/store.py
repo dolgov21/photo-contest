bef5b17911fdf4b3702d2f0ac080c0878714445c
Dolgov Denis <dindilon2000@gmail.com>
2025-09-29 00:39:51 +0300

    store/ cleaned up by ruff

--- app/store/store.py
+++ app/store/store.py
@@ -3,9 +3,11 @@ import typing
 if typing.TYPE_CHECKING:
     from app.web.app import Application
 
+
 class Store:
     def __init__(self, app: "Application"):
         self.app = app
+
 
 def setup_store(app: "Application") -> None:
     app.store = Store(app)
