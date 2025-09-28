e77a068274fd6e6218417824bcd87be1b14d7a1c
Dolgov Denis <dindilon2000@gmail.com>
2025-09-29 00:28:31 +0300

    prepare infrastructure

---
+++ app/base/base_accessor.py
@@ -0,0 +1,20 @@
+from typing import TYPE_CHECKING
+from abc import abstractmethod
+
+if TYPE_CHECKING:
+    from app.web.app import Application
+
+class BaseAccessor:
+    def __init__(self, app: "Application", *args, **kwargs):
+        self.app = app
+
+        app.on_startup.append(self.connect)
+        app.on_shutdown.append(self.disconnect)
+
+    @abstractmethod
+    async def connect(self):
+        return
+
+    @abstractmethod
+    async def disconnect(self):
+        return
--- app/store/store.py
+++ app/store/store.py
@@ -1,5 +1,11 @@
-class Store:
-    def __init__(self, *args, **kwargs):
-        from app.users.accessor import UserAccessor
+import typing
 
-        self.user = UserAccessor(self)
+if typing.TYPE_CHECKING:
+    from app.web.app import Application
+
+class Store:
+    def __init__(self, app: "Application"):
+        self.app = app
+
+def setup_store(app: "Application") -> None:
+    app.store = Store(app)
