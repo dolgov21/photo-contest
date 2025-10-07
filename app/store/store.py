fd698ff2f3f4a49fdc3f168fc23e2a8b3d38ef7d
Dolgov Denis <dindilon2000@gmail.com>
2025-10-07 11:41:24 +0300

    add TournamentService to the Store

--- app/store/store.py
+++ app/store/store.py
@@ -8,9 +8,11 @@ class Store:
     def __init__(self, app: "Application"):
         from app.bot.accessor import BotAccessor
         from app.db.accessor import DatabaseAccessor
+        from app.services.tournament import TournamentService
 
         self.bot = BotAccessor(app)
         self.db = DatabaseAccessor(app)
+        self.services = TournamentService(app)
 
 
 def setup_store(app: "Application"):
