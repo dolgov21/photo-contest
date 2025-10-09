5bbdf182a83f6099b780f90b86eaf77468a1037d
Dolgov Denis <dindilon2000@gmail.com>
2025-10-09 21:48:04 +0300

    add aiohttp logging

---
+++ app/admin/schemes.py
@@ -0,0 +1,6 @@
+from marshmallow import Schema, fields
+
+class AdminSchema(Schema):
+    id = fields.Int(dump_only=True)
+    email = fields.Str(required=True)
+    password = fields.Str(required=True)
---
+++ app/admin/views.py
@@ -0,0 +1,44 @@
+import hashlib
+from aiohttp_apispec import request_schema, response_schema
+from aiohttp_session import new_session
+
+from app.web.app import View
+from app.web.utils import json_response, error_json_response
+from app.admin.schemes import AdminSchema
+from app.web.schemes import OkResponseSchema
+from app.web.mw import HTTP_ERROR_CODES
+
+class AdminLoginView(View):
+    @request_schema(AdminSchema)
+    @response_schema(OkResponseSchema, 200)
+    async def post(self):
+        email = self.data["email"]
+        password = self.data["password"]
+
+        admin = await self.store.admins.get_by_email(email)
+        hashed_password = hashlib.md5(password.encode()).hexdigest()
+
+        if not admin or admin.password != hashed_password:
+            return error_json_response(
+                http_status=403,
+                status=HTTP_ERROR_CODES[403],
+                message="Invalid credentials"
+            )
+        
+        session = await new_session(request=self.request)
+        session["admin"] = {
+            "id": admin.id,
+            "email": admin.email,
+        }
+
+        return json_response(
+            {"id": admin.id, "email": admin.email}
+        )
+
+
+class AdminCurrentView(View):
+    async def get(self):
+        admin = self.request["admin"] 
+        return json_response(
+            admin
+        )
